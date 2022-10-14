cd11fcb5dcca152f0a66a3a0a1496ecf5c0c3884
Rorigo Brayner <rodrigo.brayner@pop-pe.rnp.br>
2022-10-13 22:04:58 -0300

    Upgrade node group to use spot and taints

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,6 +1,7 @@
 resource "aws_eks_cluster" "eks" {
     name     = "EKS_Demo"
     role_arn = aws_iam_role.eks.arn
+    version = "1.23"
 
     vpc_config {
         subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id]
@@ -21,12 +22,15 @@ resource "aws_eks_cluster" "eks" {
     ]
 }
 
-resource "aws_eks_node_group" "main" {
+resource "aws_eks_node_group" "eks_cluster_nodegroup_ondemand" {
   cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "EKS_Demo_Node_Group"
+  node_group_name = "eks_cluster_nodegroup_ondemand"
   node_role_arn   = aws_iam_role.node_group.arn
   subnet_ids      = aws_subnet.private_for_eks_node_group[*].id
 
+  labels = {
+    type_of_nodegroup = "on_demand_untainted"
+  }
   scaling_config {
     desired_size = 1
     max_size     = 2
@@ -46,10 +50,59 @@ resource "aws_eks_node_group" "main" {
   ]
 }
 
-output "endpoint" {
-  value = aws_eks_cluster.eks.endpoint
+# https://www.linkedin.com/pulse/eks-cluster-using-terraform-shishir-khandelwal/
+resource "aws_eks_node_group" "eks_cluster_nodegroup_spot" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "eks_cluster_nodegroup_spot"
+  node_role_arn   = aws_iam_role.node_group.arn
+  subnet_ids      = aws_subnet.private_for_eks_node_group[*].id
+  capacity_type = "SPOT"
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+   
+  labels = {
+    type_of_nodegroup = "spot_untainted"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
 }
 
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.eks.certificate_authority[0].data
+resource "aws_eks_node_group" "eks_cluster_nodegroup_spot_tainted" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "eks_cluster_nodegroup_spot_tainted"
+  node_role_arn   = aws_iam_role.node_group.arn
+  subnet_ids      = aws_subnet.private_for_eks_node_group[*].id
+  capacity_type = "SPOT"
+  instance_types = ["t3a.small"]
+
+  taint {
+    key = "jobs"
+    value = "true"
+    effect = "NO_SCHEDULE"
+  }
+  labels = {  
+     type_of_nodegroup = "spot_tainted"
+  }
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+output "access_cluster" {
+  value = "To access the cluster, run 'aws eks --region us-east-1 update-kubeconfig --name ${aws_eks_cluster.eks.name}'"
 }
